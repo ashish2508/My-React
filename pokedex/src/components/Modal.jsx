fbea8058750ab5bc03f86263b94e07659176adb4
ashish2508 <jha250805@gmail.com>
2024-12-16 02:34:35 +0530

    added a portal div

--- pokedex/src/components/Modal.jsx
+++ pokedex/src/components/Modal.jsx
@@ -1,4 +1,14 @@
-export default function Modal() {
-  return
+import ReactDom from "react-dom";
 
+export default function Modal(props) {
+  const {children}=props
+	return ReactDom.createPortal(
+		<div className="modal-container">
+			<button onClick={handleCloseModal} className="modal-under-lay">
+				<div className="modal-content">{children}</div>
+			</button>
+		</div>,
+		document.getElementById("portal")
+	);
 }
+//3:37:32
