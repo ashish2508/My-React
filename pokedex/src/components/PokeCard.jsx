079947163210bf3c5ce5345030a90326259a4e09
ashish2508 <jha250805@gmail.com>
2024-12-15 18:01:24 +0530

    added the storing in localStrage

--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -6,24 +6,17 @@ export function PokeCard(props) {
 	const [data, setData] = useState(null);
 	const [loading, setloading] = useState(false);
 	useEffect(() => {
-		//if looading, then exit
 		if (loading || !localStorage) {
 			return;
 		}
-		//check if the selected pokemon information is available in the cache
-		//1.define the cache
 		let cache = {};
 		if (localStorage.getItem("pokedex")) {
 			cache = JSON.parse(localStorage.getItem("pokedex"));
 		}
-		//2.check if the selected pokemon is in the cache, otherwise fetch from the API
 		if (selectedPokemon in cache) {
-			//read from cache
-
 			setData(cache[selectedPokemon]);
 			return;
 		}
-		//if we fetch from the API, make sure to store it the cache so that it won't make any api calls and will rather search in the cache(local storage) for the next time we search
 		async function fetchPokemonData() {
 			setloading(true);
 			try {
@@ -35,16 +28,16 @@ export function PokeCard(props) {
 					throw new Error(`Failed to fetch data for ${selectedPokemon}`);
 				}
 				const pokemonData = await res.json();
-        setData(pokemonData)
-        cache[selectedPokemon]=pokemonData;
-        localStorage.setItem("pokedex",JSON.stringify(cache))
+				setData(pokemonData);
+				cache[selectedPokemon] = pokemonData;
+				localStorage.setItem("pokedex", JSON.stringify(cache));
 			} catch (err) {
 				console.log(err.message);
 			} finally {
 				setloading(false);
 			}
 		}
-    fetchPokemonData()
+		fetchPokemonData();
 	}, [selectedPokemon]);
 
 	return <div></div>;
