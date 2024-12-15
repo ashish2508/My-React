4e603f0f49cddaed23d9a21b0f709c2f8492359d
ashish2508 <jha250805@gmail.com>
2024-12-16 00:24:14 +0530

    late night

--- pokedex/src/components/Modal.jsx
+++ pokedex/src/components/Modal.jsx
@@ -1,6 +1,4 @@
-export function Modal() {
+export default function Modal() {
   return
 
 }
-
-
--- pokedex/src/main.jsx
+++ pokedex/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import  App  from "./App.jsx";
-import './index.css'
-import './fanta.css'
+import App from "./App.jsx";
+import "./fanta.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
