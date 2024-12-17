d5b43d3f9b37dea3224db55ec9c1288ff1752412
ashish2508 <jha250805@gmail.com>
2024-12-18 04:07:32 +0530

    Added Side Nav's state

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-import Header  from "./components/Header";
+import Header from "./components/Header";
 import PokeCard from "./components/PokeCard";
 import SideNav from "./components/SideNav";
 function App() {
 	const [selectedPokemon, setSelectedPokemon] = useState(0);
+	const [showSideMenu, setShowSideMenu] = useState(true);
+
+	function handleToggleMenu() {
+		setShowSideMenu(!showSideMenu);
+	}
 	return (
 		<>
-			<Header />
+			<Header handleToggleMenu={handleToggleMenu} />
 			<SideNav
 				selectedPokemon={selectedPokemon}
 				setSelectedPoekmon={setSelectedPokemon}
+				showSideMenu={showSideMenu}
+				handleToggleMenu={handleToggleMenu}
 			/>
 			<PokeCard selectedPokemon={selectedPokemon} />
 		</>
