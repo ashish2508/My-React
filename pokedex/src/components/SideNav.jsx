d1da322824a7eb346ce1dcd2a9b0048846723d96
ashish2508 <jha250805@gmail.com>
2024-12-18 04:09:58 +0530

    Created the side nav for smaller screens so that the search bar can appear only on clicking

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,16 +1,50 @@
-/* eslint-disable react/jsx-key */
+import { useState } from "react";
 import { first151Pokemon, getFullPokedexNumber } from "../utils";
-export default function SideNav() {
+export default function SideNav(props) {
+	const { selectedPokemon, setSelectedPoekmon, handleToggleMenu,showSideMenu } = props;
+	const [searchValue, setSearchValue] = useState("");
+	const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+		if (getFullPokedexNumber(eleIndex).includes(searchValue.toLowerCase())) {
+			return true;
+		}
+		if (ele.toLowerCase().includes(searchValue.toLowerCase())) {
+			return true;
+		}
+
+		return false;
+	});
+
 	return (
-		<nav>
-			<div className={"header"}>
-        <h1 className="text-gradient">Pokedex</h1>
-      </div>
-      <input type="text" />
-			{first151Pokemon.map((pokemon, pokemonIndex) => {
+		<nav className={" " + (!showSideMenu ? "open" : "")}>
+			<div className={"header " + (!showSideMenu ? "open" : "")}>
+				<button className="open-nav-button" onClick={handleToggleMenu}>
+					←
+				</button>
+				<h1 className="text-gradient">Pokédex</h1>
+			</div>
+			<input
+				placeholder="E.g. 001 or Bulbasaur"
+				type="text"
+				value={searchValue}
+				onChange={(e) => {
+					setSearchValue(e.target.value);
+				}}
+			/>
+			{filteredPokemon.map((pokemon, pokemonIndex) => {
+				const truePokedexNumber = first151Pokemon.indexOf(pokemon);
 				return (
-					<button key={pokemonIndex} className={"nav-card"}>
-						<p>{getFullPokedexNumber(pokemonIndex)}</p>
+					<button
+						onClick={() => {
+							setSelectedPoekmon(truePokedexNumber);
+              handleToggleMenu();//closed the nav
+						}}
+						key={pokemonIndex}
+						className={
+							"nav-card" +
+							(pokemonIndex === selectedPokemon ? "nav-card-selected" : " ")
+						}
+					>
+						<p>{getFullPokedexNumber(truePokedexNumber)}</p>
 						<p>{pokemon}</p>
 					</button>
 				);
